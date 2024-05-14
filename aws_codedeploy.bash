1321493a8226c429ee71e6696a69faf3c6d593de
Koushal Saini <sainikoushal@gmail.com>
2024-05-14 15:22:22 +0530

    asd

--- aws_codedeploy.bash
+++ aws_codedeploy.bash
@@ -4,8 +4,8 @@ sudo apt install ruby-full -y
 sudo apt install wget -y
 cd /home/ubuntu
 #region can be changed below
-wget https://aws-codedeploy-us-east-2.s3.us-east-1.amazonaws.com/latest/install
+wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
-systemctl status codedeploy-agent
-systemctl enable codedeploy-agent
+sudo systemctl status codedeploy-agent
+sudo systemctl enable codedeploy-agent
