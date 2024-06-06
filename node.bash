26b6c364e27eecfe2fae3cdbf5aef8c430fd1645
KoushalSaini066 <koushal.saini@operisoft.com>
2024-06-06 14:22:14 +0530

    node and aws cli

--- node.bash
+++ node.bash
@@ -1,8 +1,10 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-source ~/.bashrc
-nvm install --lts
-npm install pm2 -g
-npm -v
-nvm -v
-node -v
-pm2 -v
+#!/bin/bash
+
+# Update package lists
+sudo apt-get update
+sudo apt install nodejs npm -y
+wget https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh && sudo bash install.sh && bash install.sh
+sudo groupadd pm2
+sudo usermod -aG pm2 ubuntu
+sudo usermod -aG pm2 root
+sudo apt update && sudo apt install sudo curl && curl -sL https://raw.githubusercontent.com/Unitech/pm2/master/packager/setup.deb.sh | sudo -E bash -
