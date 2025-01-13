1e89beca09c5882f5f4b15ea3ef5a43a47112536
Koushal Saini <koushal.saini@operisoft.com>
2025-01-13 12:52:21 +0530

    Create github.bash

---
+++ github.bash
@@ -0,0 +1,4 @@
+sudo apt install gh git-all
+
+echo "Installation is completed - please add github username an password. you can select https and use the code mentioned to setup the git for https."
+gh auth login
