50092948f4155958c42b98177f8eb0d0246d36d9
Koushal Saini <koushal.saini@operisoft.com>
2025-01-15 14:59:30 +0530

    added usermod

--- DockerInstallation.sh
+++ DockerInstallation.sh
@@ -20,3 +20,8 @@ sudo docker run hello-world
 
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
+
+for user in /home/*; do
+    username=$(basename "$user")
+    sudo usermod -aG docker "$username"
+done
